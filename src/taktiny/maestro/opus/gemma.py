5ee6d04abfb47c6b2844634e43d68fa1db390c18
ShinapriLN <shinapri.2003.1@gmail.com>
2026-08-11 18:49:54 +0700

    refactor: move a generic transformer file which contain common module about transformer to cosettes/transformers/

--- src/taktiny/maestro/opus/gemma.py
+++ src/taktiny/maestro/opus/gemma.py
@@ -19,7 +19,7 @@ import typing as tp
 import jax.numpy as jnp, jax
 
 from taktiny.maestro._livret import repertoire
-from taktiny.cosettes._ordinario import (
+from taktiny.cosettes.transformers._ordinario import (
     TransformerCausalLM,
     TransformerConditionalGeneration,
     DiffusionLM,
